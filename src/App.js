aa39068746d6b0870005dfbf6e53c44443ef77ac
elz0z <ghost.lover0@yahoo.com>
2024-04-07 14:06:54 +0200

    react array in state assignment solution

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ShoppingCart from "./ShoppingCart"
+function App() {
+  return (
+    <div className="App">
+      <ShoppingCart />
+    </div>
+  );
+}
+
+export default App;
